37a8e2f1a2b46a1e0fc531f6e787184dc058973a
skishimaru <shelbie.ishimaru@noaa.gov>
2024-09-14 13:25:17 -1000

    Renamed Tuesday's class script and created online lecture script

---
+++ Week_03/Scripts/Week03B_script.R
@@ -0,0 +1,9 @@
+# MBIO 612: Week 3B, ggplot2
+# Created by: Shelbie Ishimaru
+# Created on: 20204-09-14
+################################################################################
+# Load Libraries ---------------------------------------------------------------
+library(palmerpenguins)
+library(tidyverse)
+library(ggplot2)
+library(here)
